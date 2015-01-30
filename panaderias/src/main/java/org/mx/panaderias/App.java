8525b7aa37db3a632c94ebf28c33cf581794ddf0
VCGDEV <vcg_cruz@hotmail.com>
2015-01-29 21:10:43 -0600

    Configuracion proyecto maven

---
+++ panaderias/src/main/java/org/mx/panaderias/App.java
@@ -0,0 +1,13 @@
+package org.mx.panaderias;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
