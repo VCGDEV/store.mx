061dbd11e3b63ec6d9b85b86aea8e336b64c02c9
VCGDEV <vcg_cruz@hotmail.com>
2015-01-29 23:45:56 -0600

    Update

--- panaderias/src/main/java/org/mx/panaderias/domain/Usuario.java
+++ panaderias/src/main/java/org/mx/panaderias/domain/Usuario.java
@@ -1,10 +1,31 @@
 package org.mx.panaderias.domain;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Id;
+import org.hibernate.validator.constraints.NotEmpty;
+/**
+* @Autor vcgdev
+* @version 1.0
+* Usuario que puede ingresar al sistema,
+* en relacion con un Beneficiario el cual tiene sus datos personales y direccion
+*/
 
-public class Usuario{
+@Entity
+@Table(name="Usuario")
+public class Usuario implements Serializable{
+	@Id
+	@Column(name="username")
+	@NotEmpty(message="Agregue el usuario")
 	private String userName;
+	@Column(name="password")
+	@NotEmpty(message="Debe establecer alguna contraseña")
 	private String password;
+	@Column(name="enabled")
 	private boolean enabled;
-	
+	@Column(name="idBeneficiario")
+	private int idBeneficiario;
 	public void setUserName(String userName){
 		this.userName=userName;
 	}
@@ -25,5 +46,13 @@ public class Usuario{
 	}
 	public boolean isEnabled(){
 		return this.enabled;
+	}
+
+	public void setIdBeneficiario(int idBeneficiario){
+		this.idBeneficiario = idBeneficiario;
+	}
+
+	public int getIdBeneficiario(){
+		return this.idBeneficiario;
 	}
 }
